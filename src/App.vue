29480fc0a4bfce77700b22762a15b5bab4e1c4cb
= <christopher.dent@opscompass.com>
2023-11-06 17:07:04 -0500

    Adds genres

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { items } from "./movies.json";
+import { computed } from "vue";
+
+/*
+ This is an Icon that you can use to represent the stars if you like
+ otherwise you could just use a simple ⭐️ emoji, or * character.
+*/
+import { StarIcon } from "@heroicons/vue/24/solid";
+
+//  const getStarCount = (movie) => {
+//     return Math.round(movie.rating);
+//  }
+
+//  Computed
+
+const getStarCount = (movie) => {
+  return Math.round(movie.rating);
+};
+
+
+const handleImageError = (event) => {
+  event.target.src = "https://cdnb.artstation.com/p/marketplace/presentation_assets/000/835/277/large/file.jpg?1618411955";
+};
+</script> 
+
+<template>
+  <br>
+  <div class="flex items-center justify-center">
+    <h1 class="text-3xl text-center">Chris Vue Movies App</h1>
+  </div>
+  <br> <br>
+
+  <div class="flex items-center justify-center">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+      <div class="max-w-sm rounded overflow-hidden shadow-lg border-dashed border-blue-500"
+           v-for="item in items">
+        <img class="w-full h-48 object-cover"
+             :src="item.image"
+             alt="Image"
+             @error="handleImageError">
+        <div class="px-6 py-4">
+
+
+          <span v-for="genre in item.genres"
+                class="inline-block px-2 py-1 text-white text-xs bg-purple-600 rounded-full mr-2 mb-2"
+                :key="genre">
+            {{ genre }}
+          </span>
+
+
+          <h3 class="font-bold text-xl mb-2">{{ item.name }}</h3>
+          <p class="text-gray-700 text-base">{{ item.description }}.</p>
+        </div>
+
+
+        <div class="flex items-center justify-between">
+          <span class="m-6">Rating: [{{ getStarCount(item) }} / 5]</span>
+          <div class="flex space-x-1 m-6">
+            <StarIcon v-for="n in getStarCount(item)"
+                      class="h-5 w-5 text-yellow-400" />
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
